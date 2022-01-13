07a747839aee6e16411f6644a380f6b85d612247
Richard Kim <Richard.kim.0000@gmail.com>
2022-01-13 14:15:25 -0800

    added more schemas

--- imgpage_backend/schemas/pin.js
+++ imgpage_backend/schemas/pin.js
@@ -9,14 +9,49 @@ export default {
       type: "string",
     },
     {
-      name: "title",
-      title: "Title",
+      name: "about",
+      title: "About",
       type: "string",
     },
     {
-      name: "title",
-      title: "Title",
+      name: "destination",
+      title: "Destination",
+      type: "url",
+    },
+    {
+      name: "category",
+      title: "Category",
       type: "string",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "userId",
+      title: "UserId",
+      type: "string",
+    },
+    {
+      name: "postedBy",
+      title: "PostedBy",
+      type: "postedBy",
+    },
+    {
+      name: "save",
+      title: "Save",
+      type: "array",
+      of: [{ type: "save" }],
+    },
+    {
+      name: "comments",
+      title: "Comments",
+      type: "array",
+      of: [{ type: "comment" }],
     },
   ],
 };
